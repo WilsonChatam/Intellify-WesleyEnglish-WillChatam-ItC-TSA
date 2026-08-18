fe2b19c306d1a1a1f00b0063478e8601d7ac0326
WilsonChatam <wilson.chatam@ascte.org>
2026-08-18 12:16:23 -0500

    Implement navigation and alert functionality
    Added navigation buttons for 'About Us' and 'Courses' pages, along with an alert for 'Hello' button.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-const homeButton = document.getElementById("linkToHome");
-homeButton.addEventListener("click", function() {
-    window.location.href = "index.html";
+const button = document.getElementById("helloButton");
+button.addEventListener("click", function() {
+    alert("This was made using JavaScript!");
 });
+
+function navBar() {
+    const homeButton = document.getElementById("homeButton");
+    homeButton.addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+
+    const aboutUsButton = document.getElementById("aboutUsButton");
+    aboutUsButton.addEventListener("click", function() {
+        window.location.href = "aboutUs.html";
+    });
+
+    const coursesButton = document.getElementById("coursesButton");
+    coursesButton.addEventListener("click", function() {
+        window.location.href = "courses.html";
+    });
+}
+
+navBar();
