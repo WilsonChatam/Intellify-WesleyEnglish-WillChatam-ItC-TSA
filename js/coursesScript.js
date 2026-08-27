f25b6f5784dab83928c7083ecbf642d195975eca
WilsonChatam <wilson.chatam@ascte.org>
2026-08-27 15:07:17 -0500

    Refactor navBar and add dropdown course link
    Refactored navigation bar functionality and added dropdown text course link toggle.

--- js/coursesScript.js
+++ js/coursesScript.js
@@ -1,18 +1,26 @@
-function navBar() {
-    const homeButton = document.getElementById("homeButton");
-    homeButton.addEventListener("click", function() {
-        window.location.href = "index.html";
-    });
+// Navigation bar
 
-    const aboutUsButton = document.getElementById("aboutUsButton");
-    aboutUsButton.addEventListener("click", function() {
-        window.location.href = "aboutUs.html";
-    });
+const aboutUsButton = document.getElementById("aboutUsButton");
+aboutUsButton.addEventListener("click", function() {
+    window.location.href = "aboutUs.html";
+});
 
-    const coursesButton = document.getElementById("coursesButton");
-    coursesButton.addEventListener("click", function() {
-        window.location.href = "courses.html";
-    });
-}
+const coursesButton = document.getElementById("coursesButton");
+coursesButton.addEventListener("click", function() {
+    window.location.href = "courses.html";
+});
 
-navBar();
+// Courses list
+
+const dropDownTextButton = document.getElementById("dropDownTextButton");
+const doesTCOLinkExist = document.getElementById("textCourseOneLink");
+dropDownTextButton.addEventListener("click", function() {
+    if (doesTCOLinkExist) {
+        doesTCOLinkExist.remove();
+    } else {
+        const textCourseOneLink = document.createElement('a');
+        textCourseOneLink.textContent = "COURSE 1";
+        textCourseOneLink.href = "../htmlCode/index.html";
+        document.body.appendChild(textCourseOneLink);
+    }
+});
