e9467d939ada2a85c1919e4a677c0c5735d3bfd2
WilsonChatam <wilson.chatam@ascte.org>
2026-08-27 15:07:41 -0500

    Refactor navigation bar event listeners
    Removed navBar function and added event listeners directly for navigation buttons.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-function navBar() {
-    const homeButton = document.getElementById("homeButton");
-    homeButton.addEventListener("click", function() {
-        window.location.href = "index.html";
-    });
+// Navigation bar
+const aboutUsButton = document.getElementById("aboutUsButton");
+aboutUsButton.addEventListener("click", function() {
+    window.location.href = "aboutUs.html";
+});
 
-    const aboutUsButton = document.getElementById("aboutUsButton");
-    aboutUsButton.addEventListener("click", function() {
-        window.location.href = "aboutUs.html";
-    });
-
-    const coursesButton = document.getElementById("coursesButton");
-    coursesButton.addEventListener("click", function() {
-        window.location.href = "courses.html";
-    });
-}
-
-navBar();
+const coursesButton = document.getElementById("coursesButton");
+coursesButton.addEventListener("click", function() {
+    window.location.href = "courses.html";
+});
