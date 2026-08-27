4b07ebd3fea139ec7740182432b2e356ebc8cd0c
WilsonChatam <wilson.chatam@ascte.org>
2026-08-27 15:06:51 -0500

    Refactor navigation event listeners in aboutUsScript

--- js/aboutUsScript.js
+++ js/aboutUsScript.js
@@ -1,18 +1,9 @@
-function navBar() {
-    const homeButton = document.getElementById("homeButton");
-    homeButton.addEventListener("click", function() {
-        window.location.href = "index.html";
-    });
+const aboutUsButton = document.getElementById("aboutUsButton");
+aboutUsButton.addEventListener("click", function() {
+    window.location.href = "aboutUs.html";
+});
 
-    const aboutUsButton = document.getElementById("aboutUsButton");
-    aboutUsButton.addEventListener("click", function() {
-        window.location.href = "aboutUs.html";
-    });
-
-    const coursesButton = document.getElementById("coursesButton");
-    coursesButton.addEventListener("click", function() {
-        window.location.href = "courses.html";
-    });
-}
-
-navBar();
+const coursesButton = document.getElementById("coursesButton");
+coursesButton.addEventListener("click", function() {
+window.location.href = "courses.html";
+});
