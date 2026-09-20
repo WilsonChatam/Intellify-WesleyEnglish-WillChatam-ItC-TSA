ac7bdc5bfbfd6593e200662cb9cc87f37a1daa38
WilsonChatam <wilson.chatam@ascte.org>
2026-09-20 15:12:34 -0500

    Refactor navigation and dropdown functionality

--- js/coursesScript.js
+++ js/coursesScript.js
@@ -2,25 +2,68 @@
 
 const aboutUsButton = document.getElementById("aboutUsButton");
 aboutUsButton.addEventListener("click", function() {
-    window.location.href = "aboutUs.html";
+    window.location.href = "../aboutUs.html";
 });
 
 const coursesButton = document.getElementById("coursesButton");
 coursesButton.addEventListener("click", function() {
-    window.location.href = "courses.html";
+    window.location.href = "../../htmlCode/courses/courses.html";
 });
 
-// Courses list
+// Div courses
 
-const dropDownTextButton = document.getElementById("dropDownTextButton");
-const doesTCOLinkExist = document.getElementById("textCourseOneLink");
-dropDownTextButton.addEventListener("click", function() {
-    if (doesTCOLinkExist) {
-        doesTCOLinkExist.remove();
+// Text
+
+let isOpenedT = false;
+const dropdownTextButton = document.getElementById("dropDownTextButton");
+const lessonOneT = document.getElementById("dDT1");
+const lessonTwoT = document.getElementById("dDT2");
+const breakOneT = document.getElementById("break1T");
+const breakTwoT = document.getElementById("break2T");
+
+function willShowT(statusT) {
+    lessonOneT.style.display = statusT;
+    lessonTwoT.style.display = statusT;
+    breakOneT.style.display = statusT;
+    breakTwoT.style.display = statusT;
+}
+
+willShowT("none");
+
+dropdownTextButton.addEventListener("click", function() {
+    if (isOpenedT == false) {
+        willShowT("inline");
+        isOpenedT = true;
     } else {
-        const textCourseOneLink = document.createElement('a');
-        textCourseOneLink.textContent = "COURSE 1";
-        textCourseOneLink.href = "../htmlCode/index.html";
-        document.body.appendChild(textCourseOneLink);
+        willShowT("none");
+        isOpenedT = false;
+    }
+});
+
+// Programming
+
+let isOpenedP = false;
+const dropDownProgrammingButton = document.getElementById("dropDownProgrammingButton");
+const lessonOneP = document.getElementById("dDP1");
+const lessonTwoP = document.getElementById("dDP2");
+const breakOneP = document.getElementById("break1P");
+const breakTwoP = document.getElementById("break2P");
+
+function willShowP(statusP) {
+    lessonOneP.style.display = statusP;
+    lessonTwoP.style.display = statusP;
+    breakOneP.style.display = statusP;
+    breakTwoP.style.display = statusP;
+}
+
+willShowP("none");
+
+dropDownProgrammingButton.addEventListener("click", function() {
+    if (isOpenedP == false) {
+        willShowP("inline");
+        isOpenedP = true;
+    } else {
+        willShowP("none");
+        isOpenedP = false;
     }
 });
